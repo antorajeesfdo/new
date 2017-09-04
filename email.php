5a416b4d664a44c9d72869f37d29eaed6b14e12e
antorajeesfdo <31616860+antorajeesfdo@users.noreply.github.com>
2017-09-04 15:05:19 +0530

    Add files via upload

---
+++ email.php
@@ -0,0 +1,30 @@
+<?php
+$to     = "antorajees.p@getfriday.com";
+$from   = $_POST['email'];
+$name   = $_POST['name'];
+$phone  = $_POST['phone'];
+$message= $_POST['comment'];
+$subject = "Inquiry From fuisz.com";
+$mail_body='<html>
+<body bgcolor="#FFFFFF" topmargin="25">
+<p> Hi, </p>
+<p>Name: ' . $name . '</p>
+<p>Email: ' . $from . '</p>
+<p>Phone: ' . $phone . '</p>
+<p>Message: ' . $message . '</p>
+<p></p>
+</body>
+</html>';
+//$headers  = "From: jfuisz@fuisz.com";
+//$headers .= "Content-type: text/html";
+
+// Always set content-type when sending HTML email
+$headers = "MIME-Version: 1.0" . "\r\n";
+$headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
+
+// More headers
+$headers .= 'From: <'. $from .'>' . "\r\n";
+$headers .= "Cc:rajesh.roshan@getfriday.com \r\n";
+mail($to, $subject, $mail_body, $headers);
+header("Location: thankyou.html");
+?>
